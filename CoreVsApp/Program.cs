5ded0117b1c8a4e220eef2e4d85e8b61ff234d2b
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2023-05-01 10:53:21 +0300

    Задание 32.5.1

--- CoreVsApp/Program.cs
+++ CoreVsApp/Program.cs
@@ -7,12 +7,17 @@ namespace CoreVsApp
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            if (app.Environment.IsStaging())
+            if (app.Environment.IsStaging() || app.Environment.IsStaging())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapGet("/", () => $"Welcome to the {app.Environment.ApplicationName}!");
+
+            app.MapGet("/config", async context =>
+            {
+                await context.Response.WriteAsync($"App name: {app.Environment.ApplicationName}. App running configuration: {app.Environment.EnvironmentName}");
+            });
 
             app.Run();
         }
