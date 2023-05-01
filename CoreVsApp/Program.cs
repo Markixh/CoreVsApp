48289748a529e40a446257d90a4f2e89faf91dd5
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2023-05-01 15:59:29 +0300

    Задание 32.5.2

--- CoreVsApp/Program.cs
+++ CoreVsApp/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Builder;
+using System.Runtime.CompilerServices;
+
 namespace CoreVsApp
 {
     public class Program
@@ -12,14 +15,57 @@ namespace CoreVsApp
                 app.UseDeveloperExceptionPage();
             }
 
-            app.MapGet("/", () => $"Welcome to the {app.Environment.ApplicationName}!");
-
-            app.MapGet("/config", async context =>
+            //Добавляем компонент для логирования запросов с использованием метода Use.
+            app.Use(async (context, next) =>
             {
-                await context.Response.WriteAsync($"App name: {app.Environment.ApplicationName}. App running configuration: {app.Environment.EnvironmentName}");
+                // Для логирования данных о запросе используем свойства объекта HttpContext
+                Console.WriteLine($"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}");
+                await next.Invoke();
             });
 
+            app.MapGet("/", () => $"Welcome to the {app.Environment.ApplicationName}!");
+
+            app.Use(async (context, next) =>
+            {
+                // Строка для публикации в лог
+                string logMessage = $"[{DateTime.Now}]: New request to http://{context.Request.Host.Value + context.Request.Path}{Environment.NewLine}";
+
+                // Путь до лога (опять-таки, используем свойства IWebHostEnvironment)
+                string logFilePath = Path.Combine(app.Environment.ContentRootPath, "Logs", "RequestLog.txt");
+
+                // Используем асинхронную запись в файл
+                await File.AppendAllTextAsync(logFilePath, logMessage);
+
+                await next.Invoke();
+            });
+
+            app.Map("/config", Config);
+
+            app.Map("/about", About);
+
             app.Run();
+        }
+
+        /// <summary>
+        ///  Обработчик для страницы Config
+        /// </summary>
+        private static void Config(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("App running configuration");
+            });
+        }
+
+        /// <summary>
+        ///  Обработчик для страницы About
+        /// </summary>
+        private static void About(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("ASP.Net Core tutorial project");
+            });
         }
     }
 }
